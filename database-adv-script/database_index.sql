f1ebcec3148b2bddf9ea1e832dc15ad657d8fd59
Messih Grmay <37037773+mesihg@users.noreply.github.com>
2025-11-01 23:53:30 +0300

    Query to retrieve bookings and user information for indexing test
    Added a query to retrieve bookings with user details.

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -5,3 +5,21 @@ CREATE INDEX idx_booking_start_date ON bookings (start_date);
 CREATE INDEX idx_property_city ON properties (city);
 CREATE INDEX idx_review_property_id ON reviews (property_id);
 CREATE INDEX idx_review_rating ON reviews (rating);
+
+
+-- Retrieve all bookings and the respective users who made those bookings.
+EXPLAIN ANALYZE
+SELECT
+  b.booking_id,
+  b.start_date,
+  b.end_date,
+  b.total_price,
+  b.status,
+  b.user_id,
+  u.first_name,
+  u.last_name,
+  u.email
+FROM
+  bookings AS b
+INNER JOIN
+  users AS u ON b.user_id = u.user_id;
